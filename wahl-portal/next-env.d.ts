561b8da583d8747fa8d30a4de4eba452da465ef3
moin7111 <101881835+moin7111@users.noreply.github.com>
2025-09-23 10:26:34 +0200

    Delete wahl-portal directory

--- wahl-portal/next-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
-
-// NOTE: This file should not be edited
-// see the Next.js TypeScript docs for more information.
-
